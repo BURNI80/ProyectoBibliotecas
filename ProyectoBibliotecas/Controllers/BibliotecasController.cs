d3a6e4c5db5c50d9d2b431f023ea1df3408bc2ad
BURNI80 <alvarogutierrezc6@gmail.com>
2023-02-17 00:58:47 +0100

    Agregar archivos de proyecto.

---
+++ ProyectoBibliotecas/Controllers/BibliotecasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoBibliotecas.Controllers
+{
+    public class BibliotecasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoBibliotecas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoBibliotecas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
