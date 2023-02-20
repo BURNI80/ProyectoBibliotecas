3132769051a6ae979f03473a5bd72429345774d4
Álvaro Gutiérrez Carnicero <alvarogutierrezc6@gmail.com>
2023-02-20 10:44:44 +0100

    Landing Page Integrada

--- ProyectoBibliotecas/Controllers/HomeController.cs
+++ ProyectoBibliotecas/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace ProyectoBibliotecas.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult LandingPage()
+        {
+            return View();
+        }
     }
 }
