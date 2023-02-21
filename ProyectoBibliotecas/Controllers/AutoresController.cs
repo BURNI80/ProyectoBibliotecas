17ee4219237e976efedbf40e2504f4a1f8193419
Álvaro Gutiérrez Carnicero <alvarogutierrezc6@gmail.com>
2023-02-21 14:11:51 +0100

    Autores Mal

---
+++ ProyectoBibliotecas/Controllers/AutoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoBibliotecas.Controllers
+{
+    public class AutoresController : Controller
+    {
+        public IActionResult IndexAutores()
+        {
+            return View();
+        }
+    }
+}
