ae3040a6add9c34e4ab9d6b5f0f76e67bb1c7bb2
BURNI80 <alvarogutierrezc6@gmail.com>
2023-03-02 01:06:58 +0100

    Panel Admin

---
+++ ProyectoBibliotecas/Controllers/AdministracionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoBibliotecas.Controllers
+{
+    public class AdministracionController : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+        public IActionResult EditPerfil()
+        {
+            return View();
+        }
+    }
+}
